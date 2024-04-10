a95c5bd74c1b6f82c63204e6afdc4a41a6b5d641
Liviu Ene <eneliviu@gmail.com>
2024-04-10 12:24:25 +0200

    Fix home-additional-content.js

--- assets/js/scriptGallery.js
+++ assets/js/scriptGallery.js
@@ -1,8 +1,1 @@
 
-
-let emptyTask = document.getElementsByClassName('task');
-newDiv = document.createElement('div');
-newDiv.innerHTML = "<h2> Not empty </h2>"
-newDiv.style.width = "300px";
-newDiv.style.backgroundColor = "yellow";
-emptyTask.appendChild(newDiv);
