6b8bf784cc8071bb3431bafde171350df30bbbf6
Liviu Ene <eneliviu@gmail.com>
2024-04-11 14:40:15 +0200

    Cleaned up the project structure / remove old files

--- assets/js/tasks.js
+++ assets/js/tasks.js
@@ -1,14 +1,39 @@
 
+
 let addTaskBtn = document.getElementById('add-task-btn');
 addTaskBtn.addEventListener('click', taskCardDisplay)
 
+let addActivityBtn = document.getElementById('add-activity-btn');
+addActivityBtn.addEventListener('click', taskActivityDisplay)
+
+let addRelevanceBtn = document.getElementById('add-relevance-btn');
+addRelevanceBtn.addEventListener('click', taskRelevanceDisplay)
+
 function taskCardDisplay(event) {
     let taskCard = document.getElementById("task-cards-section");
-    console.log(taskCard)
     if(taskCard.style.display === 'none'){
-        taskCard.style.display = 'flex';
+        taskCard.style.display = 'block';
     } else{
         taskCard.style.display = 'none';
     }
 }
+
+function taskActivityDisplay(event) {
+    let taskActiv = document.getElementById("task-activity-div");
+    if(taskActiv.style.display === 'none'){
+        taskActiv.style.display = 'block';
+    } else{
+        taskActiv.style.display = 'none';
+    }
+}
+
+function taskRelevanceDisplay(event) {
+    let taskRelev = document.getElementById("task-importance-div");
+    if(taskRelev.style.display === 'none'){
+        taskRelev.style.display = 'block';
+    } else{
+        taskRelev.style.display = 'none';
+    }
+}
+
 
