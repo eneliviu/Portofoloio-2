51a8ff9961888ee450ded87715afbb0c0cf669d2
Liviu Ene <eneliviu@gmail.com>
2024-04-13 19:51:14 +0200

    Add only one button selection function

--- assets/js/tasks.js
+++ assets/js/tasks.js
@@ -1,40 +1,20 @@
 
 function taskCardDisplay(event) {
     let taskCard = document.getElementById("task-cards-section");
-    if (taskCard.style.display !== 'block') {
-        taskCard.style.display = 'block';
-    } else {
-        taskCard.style.display = 'none';
-    }
+    taskCard.style.display = (taskCard.style.display !== 'block' ? 'block' : 'none');
 }
 
 function taskActivityDisplay(event) {
     let taskActiv = document.getElementById("task-activity-div");
-    if (taskActiv.style.display !== 'flex') {
-        taskActiv.style.display = 'flex';
-    } else {
-        taskActiv.style.display = 'none';
-    }
+    taskActiv.style.display = (taskActiv.style.display !== 'flex' ? 'flex' : 'none');
 }
 
 function taskRelevanceDisplay(event) {
-    let taskRelev = document.getElementById("task-importance-div");
-    if (taskRelev.style.display !== 'flex') {
-        taskRelev.style.display = 'flex';
-    } else {
-        taskRelev.style.display = 'none';
-    }
+    let taskRelevance = document.getElementById("task-importance-div");
+    taskRelevance.style.display = (taskRelevance.style.display !== 'flex' ? 'flex' : 'none');
 }
 
-document.getElementById('add-task-btn').addEventListener('click', taskCardDisplay);
-document.getElementById('add-activity-btn').addEventListener('click', taskActivityDisplay);
-document.getElementById('add-relevance-btn').addEventListener('click', taskRelevanceDisplay);
-
-
-
-
-document.getElementById('add-task-ok-btn').addEventListener('click', addTaskToSet);
-function addTaskToSet(event) {
+function addTaskToTaskList(event) {
 
     let taskName = document.getElementById("task-name-field").value;
     let taskDescription = document.getElementById("task-description-field").value;
@@ -44,28 +24,53 @@ function addTaskToSet(event) {
     newTask.id = Date.now();
 
     let newTaskType = document.createElement("div");
+    newTaskType.id = newTask.id;
+
+    let newTCkb = document.createElement("checkbox");
+    newTCkb.innerHTML
 
     newTask.innerHTML = `<div>
                             <h2>${taskName}</h2>
                             <h3>${taskDescription}</h3>
-                        </div>`;
-    newTaskType.innerHTML = `<div>
-                                <span>${'Category'}</span>
-                                <span>${'Relevance'}</span>
-                            </div>`;
-    newTaskType.style.color = "red";
+                        </div>
+                        `;
+    newTaskType.innerHTML = `<span id="activity-span">${'Category'}</span>
+                            <span> | </span>
+                            <span id="relevance-span">${'Relevance'}</span>
+                            `;
+    newTaskType.className = 'innerHTML-task-category';
 
     newTask.appendChild(newTaskType);
     taskList.appendChild(newTask);
 
-    // close settings:
+    //----- AT EXIT: ------------ 
+    // Close settings and collapse fields:
     document.getElementById('add-task-btn').click();
+    document.getElementById('add-activity-btn').click();
+    document.getElementById('add-relevance-btn').click();
 
-    // reset default text entry:
+    // Reset default text entry::
     document.getElementById("task-name-field").value = '';
     document.getElementById("task-description-field").value = '';
-    document.getElementById('add-task-btn').addEventListener('click', taskList);
+    document.getElementById("task-description-field").value = '';
+
+    // Reset background colors for the category buttons:
+    let activityBtns = document.getElementById("task-activity-div").children;
+    for (let i = 0; i < activityBtns.length; i++) {
+        activityBtns[i].style.backgroundColor = '#fafafa';
+    }
+    let relevanceBtns = document.getElementById("task-importance-div").children;
+    for (let i = 0; i < relevanceBtns.length; i++) {
+        relevanceBtns[i].style.backgroundColor = '#fafafa';
+    }
 }
+
+//=================================================================================
+
+document.getElementById('add-task-btn').addEventListener('click', taskCardDisplay);
+document.getElementById('add-activity-btn').addEventListener('click', taskActivityDisplay);
+document.getElementById('add-relevance-btn').addEventListener('click', taskRelevanceDisplay);
+document.getElementById('add-task-ok-btn').addEventListener('click', addTaskToTaskList);
 
 /*document.getElementsByClassName('remove-task-btn').addEventListener('click', removeTasks);
 function removeTasks(event){
@@ -73,8 +78,47 @@ function removeTasks(event){
 }
 */
 
-function taskList(event) {
-    let taskListContainer = document.getElementById("text-container");
-    taskListContainer.style.display = (taskListContainer.style.display !== 'flex' ? 'flex' : 'none');
+//===================================================================================================//
+//--------------- Handle events for Task Category buttons:
+//https://stackoverflow.com/questions/71346490/how-do-i-make-only-one-button-can-be-selected-at-time
+
+const colorActivitiesList = ['rgba(184, 70, 29, .63)', 'rgba(35, 145, 188, .63)',
+    'rgba(140, 53, 211, .63)', 'rgba(37, 106, 29, .63)']
+var colorActivities = [];
+let relevanceBtns = document.getElementById('task-importance-div').children;
+for (let i = 0; i < relevanceBtns.length; i++) {
+    relevanceBtns[i].addEventListener('click', function (event) {
+        for (let j = 0; j < relevanceBtns.length; j++) {
+            relevanceBtns[j].classList.remove('selected');
+            relevanceBtns[j].style.backgroundColor = '';
+        }
+        this.style.backgroundColor = colorRelevanceList[i];
+        colorRelevance[0] = this.style.backgroundColor;
+    });
+   
 }
+
+const colorRelevanceList = ['rgba(227, 19, 19, .63)', 'rgba(215, 131, 20, .63)',
+    'rgba(156, 129, 223, 0.993)']
+var colorRelevance = [];
+let activityBtns = document.getElementById('task-activity-div').children;
+for (let i = 0; i < activityBtns.length; i++) {
+    activityBtns[i].addEventListener('click', function (event) {
+        for (let j = 0; j < activityBtns.length; j++) {
+            activityBtns[j].classList.remove('selected');
+            activityBtns[j].style.backgroundColor = '';
+        }
+        this.style.backgroundColor = colorActivitiesList[i];
+        colorActivities[0] = this.style.backgroundColor;
+    });  
+}
+
+
+////document.getElementById("activity-span").style.color = colorActivities[0];
+//document.getElementById("relevance-span").style.color = colorRelevance[0];
+
+//===================================================================================================//
+
+
+
 
